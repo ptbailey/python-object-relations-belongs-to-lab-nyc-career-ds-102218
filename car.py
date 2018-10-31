f0db59706e266cd6951bfd2faab13b22b792c362
Patrina Bailey <p.t.bailey04@gmail.com>
2018-10-31 09:10:35 -0400

    done

--- car.py
+++ car.py
@@ -8,3 +8,37 @@ class Car:
         self._year = year
         self._owner = owner
         Car._all.append(self)
+
+    @property
+    def make(self):
+        return self._make
+    @make.setter
+    def make(self,make):
+        self._make = make
+
+    @property
+    def model(self):
+        return self._model
+    @model.setter
+    def model(self,model):
+        self._model = model
+
+    @property
+    def year(self):
+        return self._year
+    @make.setter
+    def year(self,year):
+        self._year = year
+
+    @property
+    def owner(self):
+        return self._owner
+    @owner.setter
+    def owner(self,owner):
+        self._owner = owner
+
+    @classmethod
+    def cars_driven_by(cls, occ):
+        dict_list = list(map(lambda car: vars(car),Car._all))
+        filtered = list(filter(lambda car: (vars(car['_owner']))['_occupation'] == occ, dict_list))
+        return list(map(lambda p: vars(p['_owner']), filtered))
--- person.py
+++ person.py
@@ -2,7 +2,39 @@
 from car import Car
 
 class Person:
-
     def __init__(self, name, occupation):
         self._name = name
         self._occupation = occupation
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self,name):
+        self._name = name
+
+    @property
+    def occupation(self):
+        return self._occupation
+    @occupation.setter
+    def occupation(self,occupation):
+        self.occupation = occupation
+
+    @classmethod
+    def has_oldest_car(cls):
+        dict_list = list(map(lambda car: vars(car),Car._all))
+        sorted_list = sorted(dict_list, key = lambda r: r['_year'])
+        return vars(sorted_list[0]['_owner'])
+
+    @classmethod
+    def drives_a(cls, car_name):
+        dict_list = list(map(lambda car: vars(car),Car._all))
+        filtered = list(filter(lambda car: car['_make'] == car_name, dict_list))
+        return list(map(lambda p: vars(p['_owner']), filtered))
+
+    def drives_same_make_as_me(self):
+        person = vars((list(filter(lambda car : vars(car)['_owner'] == self, Car._all))).pop())
+        person_car_name = person['_make']
+        listt = Person.drives_a(person_car_name)
+        listt.pop(listt.index(vars(self)))
+        return listt
